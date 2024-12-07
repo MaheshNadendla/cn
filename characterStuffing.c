e6b0ca18055cf9cf7dc2726d0aae2c6bec5d5884
MaheshNadendla <maheshnadendla147@gmail.com>
2024-12-08 01:10:39 +0530

    next

--- characterStuffing.c
+++ characterStuffing.c
@@ -1,92 +1,107 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-void charc(void);
-void main()
+
+void charStuff(void);
+
+int main()
 {
     int choice;
     while (1)
     {
-        printf("\n\n\n1.character stuffing");
-        printf("\n\n2.exit");
-        printf("\n\n\nenterchoice");
+        printf("\n");
+        printf("\n       <------- Enter Below Choice -------> \n");
+        printf(".....................................................\n");
+        printf(".                                                   .\n");
+        printf(".   1. Character stuffing     :           -> 1      .\n");
+        printf(".   2. Exit                   :           -> 2      .\n");
+        printf(".....................................................\n");
+        printf("\nEnter choice : ");
         scanf("%d", &choice);
-        printf("%d", choice);
-        if (choice > 2)
-            printf("\n\n invalid option ...please renter");
-        switch (choice)
-        {
-        case 1:
-            charc();
-            break;
-        case 2:
-            _exit(0);
+
+        if (choice == 1){
+            charStuff();
         }
+        else if(choice == 2){
+            break;
+        }
+        else{
+            printf("\n\nInvalid option... please re-enter.\n");
+        }
+        
     }
+    return 0;
 }
-void charc(void)
+
+void charStuff(void)
 {
-    clrscr();
-    char c[50], d[50], t[50];
+    char c[50], d[100], t[50]; // Increased `d` size for safety
     int i, m, j;
-    printf("enter the number of characters\n");
+
+    printf("Enter the number of characters (max 50) : ");
     scanf("%d", &m);
-    printf("\n enter the characters\n");
-    for (i = 0; i < m + 1; i++)
+
+    if (m <= 0 || m > 50)
+    {
+        printf("\nInvalid number of characters. Please try again.\n");
+        return;
+    }
+
+    printf("Enter the characters : ");
+    getchar(); // To consume leftover newline character
+    for (i = 0; i < m; i++)
     {
         scanf("%c", &c[i]);
     }
-    printf("\n original data\n");
-    for (i = 0; i < m + 1; i++)
+
+    printf("\nOriginal data : ");
+    for (i = 0; i < m; i++)
         printf("%c", c[i]);
+
+    // Start stuffing
     d[0] = 'd';
     d[1] = 'l';
     d[2] = 'e';
     d[3] = 's';
     d[4] = 't';
     d[5] = 'x';
-    for (i = 0, j = 6; i < m + 1; i++, j++)
+
+    for (i = 0, j = 6; i < m; i++, j++)
     {
-        if ((c[i] == 'd' && c[i + 1] == 'l' && c[i + 2] == 'e'))
+        if (c[i] == 'd' && c[i + 1] == 'l' && c[i + 2] == 'e')
         {
-            d[j] = 'd';
-            j++;
-            d[j] = 'l';
-            j++;
-            d[j] = 'e';
-            j++;
-            m = m + 3;
+            d[j++] = 'd';
+            d[j++] = 'l';
+            d[j++] = 'e';
         }
         d[j] = c[i];
     }
-    m = m + 6;
-    m++;
-    d[m] = 'd';
-    m++;
-    d[m] = 'l';
-    m++;
-    d[m] = 'e';
-    m++;
-    d[m] = 's';
-    m++;
-    d[m] = 't';
-    m++;
-    d[m] = 'x';
-    m++;
-    printf("\n\n transmitted data: \n");
-    for (i = 0; i < m; i++)
-    {
+
+    d[j++] = 'd';
+    d[j++] = 'l';
+    d[j++] = 'e';
+    d[j++] = 's';
+    d[j++] = 't';
+    d[j++] = 'x';
+
+    printf("\nTransmitted data : ");
+    for (i = 0; i < j; i++)
         printf("%c", d[i]);
-    }
-    for (i = 6, j = 0; i < m - 6; i++, j++)
+
+    // Start unstuffing
+    int receivedLength = j;
+    for (i = 6, j = 0; i < receivedLength - 6; i++, j++)
     {
-        if (d[i] == 'd' && d[i + 1] == 'l' && d[i + 2] == 'e' && d[i + 3] == 'd' && d[i + 4] == 'l' && d[i + 5] == 'e')
-            i = i + 3;
+        if (d[i] == 'd' && d[i + 1] == 'l' && d[i + 2] == 'e' &&
+            d[i + 3] == 'd' && d[i + 4] == 'l' && d[i + 5] == 'e')
+        {
+            i += 3; // Skip the stuffed part
+        }
         t[j] = d[i];
     }
-    printf("\n\nreceived data:");
+
+    printf("\nReceived data : ");
     for (i = 0; i < j; i++)
-    {
         printf("%c", t[i]);
-    }
+
+    printf("\n");
 }
