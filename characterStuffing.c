d4962cc632642ad8fc7f6d58f2466b8049be0a67
MaheshNadendla <maheshnadendla147@gmail.com>
2025-01-02 12:26:35 +0530

    next

--- characterStuffing.c
+++ characterStuffing.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void charStuff(void);
+void bitStuff(void);
 
 int main()
 {
@@ -13,28 +14,35 @@ int main()
         printf(".....................................................\n");
         printf(".                                                   .\n");
         printf(".   1. Character stuffing     :           -> 1      .\n");
-        printf(".   2. Exit                   :           -> 2      .\n");
+        printf(".   2. Bit stuffing           :           -> 2      .\n");
+        printf(".   3. Exit                   :           -> 3      .\n");
         printf(".....................................................\n");
         printf("\nEnter choice : ");
         scanf("%d", &choice);
 
-        if (choice == 1){
+        if (choice == 1)
+        {
             charStuff();
         }
-        else if(choice == 2){
+        else if (choice == 2)
+        {
+            bitStuff();
+        }
+        else if (choice == 3)
+        {
             break;
         }
-        else{
+        else
+        {
             printf("\n\nInvalid option... please re-enter.\n");
         }
-        
     }
     return 0;
 }
 
 void charStuff(void)
 {
-    char c[50], d[100], t[50]; // Increased `d` size for safety
+    char c[50], d[100], t[50];
     int i, m, j;
 
     printf("Enter the number of characters (max 50) : ");
@@ -102,6 +110,90 @@ void charStuff(void)
     printf("\nReceived data : ");
     for (i = 0; i < j; i++)
         printf("%c", t[i]);
+
+    printf("\n");
+}
+
+void bitStuff(void)
+{
+    int data[50], stuffed[100], unstuffed[50];
+    int n, i, j = 0, k, count = 0;
+
+    printf("Enter the number of bits (max 50): ");
+    scanf("%d", &n);
+
+    if (n <= 0 || n > 50)
+    {
+        printf("\nInvalid number of bits. Please try again.\n");
+        return;
+    }
+
+    printf("Enter the bits (0 or 1): ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &data[i]);
+        if (data[i] != 0 && data[i] != 1)
+        {
+            printf("\nInvalid bit. Only 0 and 1 are allowed.\n");
+            return;
+        }
+    }
+
+    printf("\nOriginal data: ");
+    for (i = 0; i < n; i++)
+        printf("%d", data[i]);
+
+    // Bit stuffing
+    for (i = 0; i < n; i++)
+    {
+        if (data[i] == 1)
+        {
+            count++;
+            if (count == 5)
+            {
+                stuffed[j++] = data[i];
+                stuffed[j++] = 0; // Insert a 0 after 5 consecutive 1s
+                count = 0;
+            }
+            else
+            {
+                stuffed[j++] = data[i];
+            }
+        }
+        else
+        {
+            stuffed[j++] = data[i];
+            count = 0;
+        }
+    }
+
+    printf("\nStuffed data: ");
+    for (i = 0; i < j; i++)
+        printf("%d", stuffed[i]);
+
+    // Bit unstuffing
+    for (i = 0, k = 0, count = 0; i < j; i++)
+    {
+        if (stuffed[i] == 1)
+        {
+            count++;
+            unstuffed[k++] = stuffed[i];
+            if (count == 5)
+            {
+                i++; // Skip the stuffed 0
+                count = 0;
+            }
+        }
+        else
+        {
+            unstuffed[k++] = stuffed[i];
+            count = 0;
+        }
+    }
+
+    printf("\nUnstuffed data: ");
+    for (i = 0; i < k; i++)
+        printf("%d", unstuffed[i]);
 
     printf("\n");
 }
